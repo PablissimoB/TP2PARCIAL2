8692a52561ffb83b6a7fd2736428188003ef11bc
Pablo Bobarini <50304993+PablissimoB@users.noreply.github.com>
2024-10-31 17:46:50 -0300

    Agregar archivos de proyecto.

---
+++ Presentation/Login.Designer.cs
@@ -0,0 +1,200 @@
+﻿namespace Agron
+{
+    partial class Login
+    {
+        /// <summary>
+        /// Variable del diseñador necesaria.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Limpiar los recursos que se estén usando.
+        /// </summary>
+        /// <param name="disposing">true si los recursos administrados se deben desechar; false en caso contrario.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Código generado por el Diseñador de Windows Forms
+
+        /// <summary>
+        /// Método necesario para admitir el Diseñador. No se puede modificar
+        /// el contenido de este método con el editor de código.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.button7 = new System.Windows.Forms.Button();
+            this.button5 = new System.Windows.Forms.Button();
+            this.button2 = new System.Windows.Forms.Button();
+            this.button1 = new System.Windows.Forms.Button();
+            this.button4 = new System.Windows.Forms.Button();
+            this.menuStrip1 = new System.Windows.Forms.MenuStrip();
+            this.configuracionToolStripMenuItem = new System.Windows.Forms.ToolStripMenuItem();
+            this.datosEmpresaToolStripMenuItem = new System.Windows.Forms.ToolStripMenuItem();
+            this.centroDeCostosToolStripMenuItem = new System.Windows.Forms.ToolStripMenuItem();
+            this.bancosToolStripMenuItem = new System.Windows.Forms.ToolStripMenuItem();
+            this.cuentasContablesToolStripMenuItem = new System.Windows.Forms.ToolStripMenuItem();
+            this.reportesToolStripMenuItem = new System.Windows.Forms.ToolStripMenuItem();
+            this.insumosToolStripMenuItem = new System.Windows.Forms.ToolStripMenuItem();
+            this.productosToolStripMenuItem = new System.Windows.Forms.ToolStripMenuItem();
+            this.menuStrip1.SuspendLayout();
+            this.SuspendLayout();
+            // 
+            // button7
+            // 
+            this.button7.Location = new System.Drawing.Point(160, 203);
+            this.button7.Name = "button7";
+            this.button7.Size = new System.Drawing.Size(146, 65);
+            this.button7.TabIndex = 20;
+            this.button7.Text = "COBROS";
+            this.button7.UseVisualStyleBackColor = true;
+            // 
+            // button5
+            // 
+            this.button5.Location = new System.Drawing.Point(8, 203);
+            this.button5.Name = "button5";
+            this.button5.Size = new System.Drawing.Size(146, 65);
+            this.button5.TabIndex = 19;
+            this.button5.Text = "PAGOS";
+            this.button5.UseVisualStyleBackColor = true;
+            // 
+            // button2
+            // 
+            this.button2.Location = new System.Drawing.Point(160, 118);
+            this.button2.Name = "button2";
+            this.button2.Size = new System.Drawing.Size(146, 65);
+            this.button2.TabIndex = 17;
+            this.button2.Text = "VENTAS ";
+            this.button2.UseVisualStyleBackColor = true;
+            // 
+            // button1
+            // 
+            this.button1.Location = new System.Drawing.Point(8, 118);
+            this.button1.Name = "button1";
+            this.button1.Size = new System.Drawing.Size(146, 65);
+            this.button1.TabIndex = 16;
+            this.button1.Text = "COMPRAS";
+            this.button1.UseVisualStyleBackColor = true;
+            // 
+            // button4
+            // 
+            this.button4.Location = new System.Drawing.Point(94, 313);
+            this.button4.Margin = new System.Windows.Forms.Padding(2);
+            this.button4.Name = "button4";
+            this.button4.Size = new System.Drawing.Size(109, 33);
+            this.button4.TabIndex = 15;
+            this.button4.Text = "Salir";
+            this.button4.UseVisualStyleBackColor = true;
+            this.button4.Click += new System.EventHandler(this.button4_Click);
+            // 
+            // menuStrip1
+            // 
+            this.menuStrip1.ImageScalingSize = new System.Drawing.Size(24, 24);
+            this.menuStrip1.Items.AddRange(new System.Windows.Forms.ToolStripItem[] {
+            this.configuracionToolStripMenuItem,
+            this.reportesToolStripMenuItem});
+            this.menuStrip1.Location = new System.Drawing.Point(0, 0);
+            this.menuStrip1.Name = "menuStrip1";
+            this.menuStrip1.Size = new System.Drawing.Size(341, 33);
+            this.menuStrip1.TabIndex = 18;
+            this.menuStrip1.Text = "menuStrip1";
+            // 
+            // configuracionToolStripMenuItem
+            // 
+            this.configuracionToolStripMenuItem.DropDownItems.AddRange(new System.Windows.Forms.ToolStripItem[] {
+            this.datosEmpresaToolStripMenuItem,
+            this.centroDeCostosToolStripMenuItem,
+            this.bancosToolStripMenuItem,
+            this.cuentasContablesToolStripMenuItem,
+            this.insumosToolStripMenuItem,
+            this.productosToolStripMenuItem});
+            this.configuracionToolStripMenuItem.Name = "configuracionToolStripMenuItem";
+            this.configuracionToolStripMenuItem.Size = new System.Drawing.Size(139, 29);
+            this.configuracionToolStripMenuItem.Text = "Configuracion";
+            // 
+            // datosEmpresaToolStripMenuItem
+            // 
+            this.datosEmpresaToolStripMenuItem.Name = "datosEmpresaToolStripMenuItem";
+            this.datosEmpresaToolStripMenuItem.Size = new System.Drawing.Size(261, 34);
+            this.datosEmpresaToolStripMenuItem.Text = "Datos Empresa";
+            // 
+            // centroDeCostosToolStripMenuItem
+            // 
+            this.centroDeCostosToolStripMenuItem.Name = "centroDeCostosToolStripMenuItem";
+            this.centroDeCostosToolStripMenuItem.Size = new System.Drawing.Size(261, 34);
+            this.centroDeCostosToolStripMenuItem.Text = "Centro de Costos";
+            // 
+            // bancosToolStripMenuItem
+            // 
+            this.bancosToolStripMenuItem.Name = "bancosToolStripMenuItem";
+            this.bancosToolStripMenuItem.Size = new System.Drawing.Size(261, 34);
+            this.bancosToolStripMenuItem.Text = "Bancos";
+            // 
+            // cuentasContablesToolStripMenuItem
+            // 
+            this.cuentasContablesToolStripMenuItem.Name = "cuentasContablesToolStripMenuItem";
+            this.cuentasContablesToolStripMenuItem.Size = new System.Drawing.Size(261, 34);
+            this.cuentasContablesToolStripMenuItem.Text = "Cuentas Contables";
+            // 
+            // reportesToolStripMenuItem
+            // 
+            this.reportesToolStripMenuItem.Name = "reportesToolStripMenuItem";
+            this.reportesToolStripMenuItem.Size = new System.Drawing.Size(98, 29);
+            this.reportesToolStripMenuItem.Text = "Reportes";
+            // 
+            // insumosToolStripMenuItem
+            // 
+            this.insumosToolStripMenuItem.Name = "insumosToolStripMenuItem";
+            this.insumosToolStripMenuItem.Size = new System.Drawing.Size(270, 34);
+            this.insumosToolStripMenuItem.Text = "Insumos";
+            // 
+            // productosToolStripMenuItem
+            // 
+            this.productosToolStripMenuItem.Name = "productosToolStripMenuItem";
+            this.productosToolStripMenuItem.Size = new System.Drawing.Size(270, 34);
+            this.productosToolStripMenuItem.Text = "Productos";
+            // 
+            // Login
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(10F, 25F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(341, 369);
+            this.Controls.Add(this.button7);
+            this.Controls.Add(this.button5);
+            this.Controls.Add(this.button2);
+            this.Controls.Add(this.button1);
+            this.Controls.Add(this.button4);
+            this.Controls.Add(this.menuStrip1);
+            this.Margin = new System.Windows.Forms.Padding(3, 4, 3, 4);
+            this.Name = "Login";
+            this.menuStrip1.ResumeLayout(false);
+            this.menuStrip1.PerformLayout();
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private Button button7;
+        private Button button5;
+        private Button button2;
+        private Button button1;
+        private Button button4;
+        private MenuStrip menuStrip1;
+        private ToolStripMenuItem configuracionToolStripMenuItem;
+        private ToolStripMenuItem datosEmpresaToolStripMenuItem;
+        private ToolStripMenuItem centroDeCostosToolStripMenuItem;
+        private ToolStripMenuItem bancosToolStripMenuItem;
+        private ToolStripMenuItem cuentasContablesToolStripMenuItem;
+        private ToolStripMenuItem reportesToolStripMenuItem;
+        private ToolStripMenuItem insumosToolStripMenuItem;
+        private ToolStripMenuItem productosToolStripMenuItem;
+    }
+}
+
